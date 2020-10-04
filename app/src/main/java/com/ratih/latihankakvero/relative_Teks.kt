35734b4c753adfb41a87239419efea11a86e9540
RatihEl <ratiheldina1234@gmail.com>
2020-10-04 10:46:29 +0700

    Initial commit

---
+++ app/src/main/java/com/ratih/latihankakvero/relative_Teks.kt
@@ -0,0 +1,11 @@
+package com.ratih.latihankakvero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class relative_Teks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative__teks)
+    }
+}
